42c47cd83acd07eed03a0e5738bbca5447a45ff6
isaac <xuhonggang@hotmail.com>
2022-04-05 21:19:26 +0800

    20220405211922

--- sync.bat
+++ sync.bat
@@ -1,7 +1,7 @@
 set filename=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
 set "filename=%filename: =0%"
 cd "c:\github\save-game"
-git pull origin main
-git add -A
-git commit -a -m %filename%
-git push origin main
+"C:\Program Files\PortableGit\bin\git.exe" pull origin main
+"C:\Program Files\PortableGit\bin\git.exe" add -A
+"C:\Program Files\PortableGit\bin\git.exe" commit -a -m %filename%
+"C:\Program Files\PortableGit\bin\git.exe" push origin main
