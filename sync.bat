9852057658206d3ec9c410334b3f25c466d78343
isaac <xuhonggang@hotmail.com>
2022-04-05 21:21:29 +0800

    20220405212124

--- sync.bat
+++ sync.bat
@@ -1,6 +1,8 @@
 set filename=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
 set "filename=%filename: =0%"
 cd "c:\github\save-game"
+"C:\Program Files\PortableGit\bin\git.exe" config user.name isaac
+"C:\Program Files\PortableGit\bin\git.exe" config user.email xuhonggang@hotmail.com
 "C:\Program Files\PortableGit\bin\git.exe" pull origin main
 "C:\Program Files\PortableGit\bin\git.exe" add -A
 "C:\Program Files\PortableGit\bin\git.exe" commit -a -m %filename%
