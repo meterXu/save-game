f8b47177bf46061a69c924f9abf48fe6b79896a3
isaac <xuhonggang@hotmail.com>
2022-04-05 21:06:28 +0800

    %filename%

---
+++ sync.bat
@@ -0,0 +1,7 @@
+set filename=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+set "filename=%filename: =0%"
+cd C:\github\save-game
+git pull origin main
+git add -A
+git commit -a -m %filename%
+git push origin main
