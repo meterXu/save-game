d10c2fc0809bbed0acdd449a97b42600724a6838
isaac <xuhonggang@hotmail.com>
2022-04-09 20:27:35 +0800

    20220409202732

--- sync.bat
+++ sync.bat
@@ -1,6 +1,6 @@
 set filename=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
 set "filename=%filename: =0%"
-cd "c:\github\save-game"
+cd %cd%
 git config user.name isaac
 git config user.email xuhonggang@hotmail.com
 git pull origin main
